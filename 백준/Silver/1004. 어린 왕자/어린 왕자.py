095bb90056e6d779bb0ff76fbbb96b18fe0e75f1
Ikbeom Jo <91316337+IkbeomJo@users.noreply.github.com>
2022-10-09 03:13:53 +0900

    [Silver III] Title: 어린 왕자, Time: 260 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/1004. 어린 왕자/어린 왕자.py
+++ 백준/Silver/1004. 어린 왕자/어린 왕자.py
@@ -1,12 +1,15 @@
-t = int(input())
-for i in range(t):
-    cnt = 0
-    x1, y1, x2, y2 = map(int, input().split())
-    n = int(input())
-    for i in range(n):
-        px, py, pr = map(int, input().split())
-        d1 = (((x1 - px) ** 2) + ((y1 - py) ** 2)) ** 0.5
-        d2 = (((x2 - px) ** 2) + ((y2 - py) ** 2)) ** 0.5
-        if (d1 < pr and d2 > pr) or (d1 > pr and d2 < pr):
-            cnt += 1
-    print(cnt)
+T = int(input())
+for _ in range(T):
+    count = 0
+    x1,y1,x2,y2 = map(int, input().split())
+    N = int(input())
+    for _ in range(N):
+        px,py,pr = map(int, input().split())
+        
+        if (x1 - px)**2 + (y1 - py)**2 < pr**2 and (x2 - px)**2 + (y2 - py)**2 < pr**2:
+            continue
+        elif (x1 - px)**2 + (y1 - py)**2 < pr**2 or (x2 - px)**2 + (y2 - py)**2 < pr**2:
+            count += 1
+            
+    print(count)
+    
