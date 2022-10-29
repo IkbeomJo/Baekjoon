ecf61ac36f3a7051a4a87c37b2aefddfde1e38d5
Ikbeom Jo <91316337+IkbeomJo@users.noreply.github.com>
2022-10-30 05:37:14 +0900

    [Gold IV] Title: N-Queen, Time: 18460 ms, Memory: 157344 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.py
+++ 백준/Gold/9663. N－Queen/N－Queen.py
@@ -1,24 +1,37 @@
-def back(n):
-    if n == N:
-        global cnt
-        cnt += 1
-        return
+def queen(N):
+    global cnt
+    cnt = 0
+    L = [None for _ in range(N)]
+    visited = [False for _ in range(N)]
+    n = 0
     
-    for i in range(N):
-        L[n] = i
-        if checkqueen(n):
-            back(n+1)
-    
-def checkqueen(n):
-    for i in range(n):
-        if L[n] == L[i] or n-i == abs(L[i] - L[n]):
-            return False
+    def back(n):
+        if n == N:
+            global cnt
+            cnt += 1
+            return
         
-    return True
+        for i in range(N):
+            if visited[i]:
+                continue
+                
+            L[n] = i
+            
+            if checkqueen(n):
+                visited[i] = True
+                back(n+1)
+                visited[i] = False
     
+    def checkqueen(n):
+        for i in range(n):
+            if L[n] == L[i] or abs(n-i) == abs(L[i] - L[n]):
+                return False
+        
+        return True
+    
+    back(n)
+    return cnt
+
 N = int(input())
-L = [0 for _ in range(N)]
-global cnt
-cnt = 0
-back(0)
-print(cnt)
+
+print(queen(N))
