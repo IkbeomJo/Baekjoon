35e816fc8466c3e3ad7e29e4c1a41d4757a27a3d
Ikbeom Jo <91316337+IkbeomJo@users.noreply.github.com>
2022-11-22 03:06:40 +0900

    [Bronze V] Title: 사분면 고르기, Time: 144 ms, Memory: 9328 KB -BaekjoonHub

---
+++ 백준/Bronze/14681. 사분면 고르기/사분면 고르기.js
@@ -0,0 +1,19 @@
+var fs = require('fs');
+const [x, y] = fs.readFileSync(0).toString().trim().split('\n').map(Number);
+
+if(x>0){
+    if(y>0){
+        console.log("1");
+    }
+    else{
+        console.log("4");
+    }
+}
+else{
+    if(y>0){
+        console.log("2");
+    }
+    else{
+        console.log("3");
+    }
+}
