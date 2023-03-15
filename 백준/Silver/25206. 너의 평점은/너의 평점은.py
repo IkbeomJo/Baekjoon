552a6fef395da3f28d06fa2dcd59259c3a591e0b
Ikbeom Jo <91316337+IkbeomJo@users.noreply.github.com>
2023-03-15 09:56:38 +0900

    [Silver V] Title: 너의 평점은, Time: 48 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/25206. 너의 평점은/너의 평점은.py
@@ -0,0 +1,14 @@
+grade_table = {'A+':4.5,'A0':4.0,
+               'B+':3.5,'B0':3.0,
+               'C+':2.5,'C0':2.0,
+               'D+':1.5,'D0':1.0,
+               'F':0.0}
+cnt = 0
+sum_result = 0
+for _ in range(20):
+    class_name, credit, grade = input().split()
+    if grade == 'P': continue
+    sum_result += grade_table[grade] * float(credit)
+    cnt += float(credit)
+
+print(sum_result/cnt)
