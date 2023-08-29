08776b6bf8101a4df20264b9164f0297c0800d85
Ikbeom Jo <91316337+IkbeomJo@users.noreply.github.com>
2023-08-29 23:42:01 +0900

    [Silver IV] Title: 수 찾기, Time: 248 ms, Memory: 141064 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.py
+++ 백준/Silver/1920. 수 찾기/수 찾기.py
@@ -2,30 +2,29 @@ import sys
 
 N = int(input())
 A = list(map(int, input().split()))
-A.sort()
 M = int(input())
-find_A = list(map(int, input().split()))
+find_list = list(map(int, input().split()))
 
-for i in find_A:
-    start = 0
-    end = len(A) - 1
-    mid = len(A) // 2
+A.sort()
+
+for i in find_list:
+    low = 0
+    high = N - 1
 
     while True:
-        if i > A[mid]:
-            start = mid
-            mid = (start + end) // 2
-
-        elif i < A[mid]:
-            end = mid
-            mid = (start + end) // 2
-
-
-        if A[mid] == i or A[mid+1] == i:
+        mid = (high + low) // 2
+        if A[mid] == i:
             print(1)
             break
-        elif start == mid or end == mid:
+
+        if low >= high:
             print(0)
             break
+
+        if A[mid] < i:
+            low = mid + 1
+
+        elif A[mid] > i:
+            high = mid - 1
 
 
